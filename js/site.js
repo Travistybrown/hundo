790d7d5a95fffe9640c2a236074434b8b807d1a7
Travistybrown <travistbrownsc@gmail.com>
2022-10-11 12:29:48 -0400

    retretwrwtertwe

--- js/site.js
+++ js/site.js
@@ -1,17 +1,41 @@
-function displayMessage(){
-  
-    let msg =  document.getElementById("message").value;
 
 
-    //alert(msg);
+function getValues(){
+    // get the value from the page 
+ let startValue = document.getElementById("startValue").value ;
+ let endValue = document.getElementById("endValue").value;
 
-    swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg,
-        }
+ // parse the values in to intengers
+startValue = parseInt(startValue);
+endValue = parseInt(endValue);
 
-    )
+
+if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+let numbers = generateNumbers(startValue,endValue);
+
+}else{
+    swal.fire({
+        icon:"error",
+        Title:"Oops....",
+        text: "Only integers are allow for Hundo!"
+    });
+}
 }
 
+
+function generateNumbers(startValue, endValue){
+    let numbers = [];
+
+    for(let i = startValue; i <= endValue; i++){
+        numbers.push(i);
+    }
+
+    return numbers;
+
+}
+
+
+function displayNumbers(){
+
+
+}
